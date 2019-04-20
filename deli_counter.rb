f65382282fc4a2a53618d8d701e4b37402907629
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-20 12:08:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |name, index|
-      current_line << "#{index}. #{name} "
+      current_line << " #{index}. #{name}"
     end
     puts current_line
   end
