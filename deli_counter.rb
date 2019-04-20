b6ccfd9ea2e7b848f0251bb57087281f76d65b44
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-20 12:01:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
     current_line = "The line is currently: "
     katz_deli.each do |index, name|
     current_line << "#{index+1}. #{name}"
+    end
   end
   puts current_line
 end
